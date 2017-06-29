7ecdfb1b3be58453dcf0ec3aadf04baaa535e06d
David Jagodowski <davejag@comcast.net>
2017-06-29 17:22:21 -0400

    Fixed issues with stats/score, but still only good for 1 round.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # psychic_game
 The game "Psychic" coded in Javascript
+.
+
+Notes: This game works through a single iteration, but it does not reset after a win or a loss.
+I reached a point of "diminishing returns" where my attempts to fix one issue would cause another. 
+I think the class could have used another week of learning Javascript, how it flows, how it handles 
+functions and the scope of local/global variables. None of this architecture was really discussed.
+An assignment that just involved creating a few functions that perform specific tasks, gather specific
+data, and returning that data to other functions would have been a good intermediary step.  
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,43 +12,40 @@ function getRandom() {
   }
 
 function getKeyChars(randomLetter) {
-	var flag = false;
+	var flag = false; // futile  attempt to stop the program from running continuously before any data is input.
   document.getElementById("guessId").addEventListener("keyup", flag=true);
   if (flag = true) {
     //console.log("Function getLetter: The random letter is: " + ranLetter);
   	var rawInput = document.getElementById("guessId").value;
-  	document.getElementById("yourGuessesId").innerHTML = (rawInput + ", "); //updates Your Guesses So Far field
+  	document.getElementById("yourGuessesId").innerHTML = rawInput; //updates Your Guesses So Far field
   	guessedLetters = rawInput;
 	  document.getElementById("guessesLeftId").innerHTML = (maxGuess - guessedLetters.length); //decrements "Guesses Left" field
     compareChars(randomLetter, rawInput);		
 	}
-  flag = false;
+  flag = false; // reset futility flag.
 }
-	//console.log("The guessedLetters are: " + guessedLetters);
-	//console.log("guessedLetters.length is: " + guessedLetters.length);
-	//if (guessedLetters.length === maxGuess) { //move to next function after collecting data
-	//	compareChars();
-	
+
 
 function compareChars(randomLetter, rawInput) {
     console.log("Function compare: The random letter is: " + ranLetter);
     console.log("Function compare: The guessedLetters are: " + guessedLetters);
   	
           //ranLetter = "j"; //for testing
-          if (ranLetter === guessedLetters[guessedLetters.length-1]) {  // compare to last letter added to array
+          if (ranLetter === guessedLetters[guessedLetters.length-1]) {  // compare random letter to last letter added to array
       	    document.getElementById("status").innerHTML = "That is Correct!";
       	    wins = wins + 1; // increment wins and update screen stats
             document.getElementById("winsId").innerHTML = wins;
            // newRound();
           
           } else {  // if incorrect guess
-              
+              console.log("ranLetter has value " + ranLetter);
               document.getElementById("status").innerHTML = "Sorry. Incorrect Guess"; 
               var numLeft = document.getElementById("guessesLeftId").innerHTML; 
-              console.log("guesses left is: " + numLeft);
-              if ( numLeft = 0) {    //if no guesses are left
+              console.log("function compareChars. Guesses left is: " + numLeft);
+              if (numLeft < 1) {    //if no guesses are left
                 losses = losses + 1;   // increment losses and update screen stats
-                document.getElementById("lossesId").innerHTML = losses; 
+                document.getElementById("lossesId").innerHTML = losses;
+                document.getElementById("status").innerHTML = "The random letter was " + ranLetter; 
                // newRound(); 
               }
           } 
@@ -63,6 +60,19 @@ function checkForWinner(wins, losses) {
   } 
 } 
 
+
+function newRound() {
+  console.log("this is the newRound function");
+  //randomLetter = "";
+  //ranLetter = ""; //stores the random letter generated.
+  //rawInput = "";
+  guessedLetters = []; //the string formed by guessed letters.
+  document.getElementById("yourGuessesId").innerHTML = "none";
+  guessesLeft = document.getElementById("guessesLeftId").innerHTML = 9;
+  document.getElementById("status").innerHTML = "Select a letter.";
+  console.log("end of function newRound");
+}
+
 /*function initGame() {
   randomLetter = "";
   wins = document.getElementById("winsId").innerHTML = 0;
@@ -71,30 +81,7 @@ function checkForWinner(wins, losses) {
   guessesSoFar = document.getElementById("yourGuessesId").innerHTML = "";
   ranLetter = ""; //stores the random letter generated.
   guessedLetters = []; //the string formed by guessed letters.
-  document.getElementById("status").innerHTML = "Select a letter."
+  document.getElementById("status").innerHTML = "Select a letter.";
 } */
-
-function newRound() {
-  console.log("this is the newRound function");
-  randomLetter = "";
-  ranLetter = ""; //stores the random letter generated.
-  rawInput = "";
-  guessedLetters = []; //the string formed by guessed letters.
-  document.getElementById("yourGuessesId").innerHTML = "none";
-  guessesLeft = document.getElementById("guessesLeftId").innerHTML = 9;
-  console.log("end of function newRound");
-}
-
-/*	for(var i=0; i<maxGuess; i++ {
-
-	})
-	console.log(rawInput[0]); 
-    console.log(rawInput[1]); 
-	console.log(rawInput[2]); 
-	console.log(rawInput[3]); 
-	console.log(rawInput[4]); 
-	console.log(rawInput[5]); 
-
- } */
 
  
